f869ae93c7e00b84ed9eb7bbf8ddfad7138576b3
jbastias <jorgebastias@hotmail.com>
2021-04-11 11:05:44 -0600

    get x, y

--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,51 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 fn main() {
  
     let mut path: String = env::current_dir().unwrap().as_os_str().to_str().unwrap().to_string();
-    let filename: &str = "/big-svg.svg";
+    // let filename: &str = "/big-svg.svg";
+    let filename: &str = "/really-big.svg";
     path.push_str(filename);
 
-    let re = Regex::new(r#"<circle cx="(\d{1,3})" cy="(\d{1,3})" r="(\d)" fill="(.*)"/>$"#).unwrap();
-    
-    let mut img = Image::new(512, 414);
+    let re_xy = Regex::new(r#"<svg width="(\d+)" height="(\d+)">$"#).unwrap();
+    let re = Regex::new(r#"<circle cx="(\d+)" cy="(\d+)" r="(\d)" fill="(.*)"/>$"#).unwrap();
 
     let lines = lines_from_file(path);
+
+    let text1: String = lines[3].to_string().trim().to_string();
+    let caps = re_xy.captures(&text1).unwrap();
+    let x: u32 = caps[1].to_owned().parse::<u32>().unwrap();
+    let y: u32 = caps[2].to_owned().parse::<u32>().unwrap();
+
+    let mut img = Image::new(x, y);
+
     for line in lines {
         let text: String = line.to_string().trim().to_string();
+
+        // let caps = re.captures(&text).unwrap();
+        // println!("{}", caps[1].to_owned().parse::<u32>().unwrap());
+        // let cx: u32 = caps[1].to_owned().parse::<u32>().unwrap();
+        // let cy: u32 = caps[2].to_owned().parse::<u32>().unwrap();
+        // let fill = &caps[4].to_owned();
+        // if fill == "white" {
+        //     img.set_pixel(cx, cy, Pixel::new(255 ,255, 255));
+        // } else {
+        //     img.set_pixel(cx, cy, Pixel::new(0 ,0, 0));
+        // }
+
         for cap in re.captures_iter(&text) {
             let cx: u32 = cap[1].to_owned().parse::<u32>().unwrap();
             let cy: u32 = cap[2].to_owned().parse::<u32>().unwrap();
             let fill = &cap[4].to_owned();
-
             if fill == "white" {
                 img.set_pixel(cx, cy, Pixel::new(255 ,255, 255));
             } else {
                 img.set_pixel(cx, cy, Pixel::new(0 ,0, 0));
             }
         }
+
     }
 
-    let _ = img.save("./big-svg.bmp");
-
+    // let _ = img.save("./big-svg.bmp");
+    let _ = img.save("./really-big.bmp");
+    
 }
 
